7b040886f123d4d83d3cb67a179b885fa02c9480
Laush <laush_x4@hotmail.com>
2019-10-11 21:58:06 -0300

    first commit

---
+++ WebApi/WebApi/Controllers/CotizacionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CotizacionController : ApiController
+    {
+        Model[] values = new Model[] {
+
+            new Model {id="dolar", source="USD", value=58.08},
+           new Model  {id="real", source="BRL", value=14.12},
+          new Model   {id="euro", source="EUR", value=64.09},
+                };
+        public IEnumerable<Model> GetAllMoneda()
+        {
+            return values;
+        }
+
+        public IHttpActionResult GetMoneda(string id)
+        {
+            var currency = values.FirstOrDefault((m) => m.id == id);
+            if (currency != null)
+            {
+                return Ok(currency);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+
+    }
+}
+
---
+++ WebApi/WebApi/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Model
+    {
+        public string id { get; set; }
+        public string source { get; set; }
+        public double value { get; set; }
+    }
+}
